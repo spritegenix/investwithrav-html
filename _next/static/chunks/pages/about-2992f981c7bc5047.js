d0a960a166e146dae82c7c6a326f0d258a11c3fc
Ashik <ashikshettyc@gmail.com>
2024-12-19 11:04:57 +0530

    initial commit

---
+++ _next/static/chunks/pages/about-2992f981c7bc5047.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{512:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(8953)}])}},function(n){n.O(0,[664,829,289,294,640,472,774,888,179],function(){return n(n.s=512)}),_N_E=n.O()}]);
