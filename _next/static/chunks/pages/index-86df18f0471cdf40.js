573e279f15727472d33770feb6e6e281f3eec8c5
Ashik <ashikshettyc@gmail.com>
2024-12-24 12:01:32 +0530

    color edit

---
+++ _next/static/chunks/pages/index-86df18f0471cdf40.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(5643)}])}},function(n){n.O(0,[738,664,261,546,289,294,949,640,472,643,774,888,179],function(){return n(n.s=5557)}),_N_E=n.O()}]);
